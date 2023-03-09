741215800c1c69367e23d6761c741e2e50b45f52
TujhFoxnime <foxnime1983@gmail.com>
2023-03-09 12:13:26 +0300

    comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
-    cout<<"Hello World\n";
+    std::string name;
+    std::cin>>name;
+    std::cout<<"Hello World "<<name<<std::endl;
     return 0;
 }
+//comments
+//comments
+//comments
+//comments
+//comments
+//comments
 
